09b13bdbbdbd96a6222df613a32de047aa50f4a4
Danil <daniil-vn@mail.ru>
2025-01-11 01:08:58 +0500

    Сравнил производительность вставки List<T> и LinkedList<T>. Для подсчета скорости использовался StopWatch.

--- TaskThirteen.Six.One/Program.cs
+++ TaskThirteen.Six.One/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+
+namespace Thirteen.Six.One
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            string[] words = File.ReadAllText("C:\\Users\\user\\Desktop\\input.txt").Split(new char[] { ' ', '\n', '\r' });
+
+            // Измеряем время вставки в List<T>
+            List<string> list = new List<string>();
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            foreach (var word in words)
+            {
+                list.Add(word);
+            }
+            sw.Stop();
+            Console.WriteLine($"Время вставки в List<T>: {sw.ElapsedMilliseconds} мс");
+
+            // Измеряем время вставки в LinkedList<T>
+            LinkedList<string> linkedList = new LinkedList<string>();
+            sw.Restart();
+            foreach (var word in words )
+            {
+                linkedList.AddLast(word);
+            }
+            sw.Stop();
+            Console.WriteLine($"Время вставки в LinkedList<T>: {sw.ElapsedMilliseconds} мс");
+        }
+    }
+}
